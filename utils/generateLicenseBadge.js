75e7ab3657b9ca07f13c065c5c1523a3dcfeed44
Obioma <obeeomaa@gmail.com>
2024-02-12 10:22:37 +0100

    Add function to generate selected badge

---
+++ utils/generateLicenseBadge.js
@@ -0,0 +1,13 @@
+// Function to create badges based on license selection.
+function generateLicenseBadge(license) {
+  switch (license) {
+    case "MIT License":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "Apache 2.0 License":
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "Mozilla Public License 2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    default:
+      return;
+  }
+}
