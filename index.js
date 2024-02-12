71ac1f7fba1265ef05b707f22cc2bd176c6ebed9
Obioma <obeeomaa@gmail.com>
2024-02-12 10:10:18 +0100

    Add function to write README file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -68,12 +68,11 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, generateMarkdown(data), (err) => console.error(err));
 }
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
