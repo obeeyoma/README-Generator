2ad0c3df24725af32c12b62fae0c30f1c589898f
Obioma <obeeomaa@gmail.com>
2024-02-12 10:01:02 +0100

    Create question objects

--- index.js
+++ index.js
@@ -5,7 +5,65 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    //   title
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    //   description
+    type: "input",
+    name: "description",
+    message: "How would you describe your project?",
+  },
+  {
+    //   installation
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions for your project?",
+  },
+  {
+    //   usage
+    type: "input",
+    name: "usage",
+    message: "How will your project be used?",
+  },
+  {
+    //   contributing
+    type: "input",
+    name: "contributing",
+    message: "What are the contribution guidelines for your project?",
+  },
+  {
+    //   tests
+    type: "input",
+    name: "test",
+    message: "Enter test instructions for your project: ",
+  },
+  {
+    //   license
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project: ",
+    choices: [
+      "MIT License",
+      "Mozilla Public License 2.0",
+      "Apache 2.0 License",
+    ],
+  },
+  {
+    //   github with link to profile
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username: ",
+  },
+  {
+    //   email
+    type: "input",
+    name: "email",
+    message: "Enter your email address: ",
+  },
 ];
 
 // function to write README file
