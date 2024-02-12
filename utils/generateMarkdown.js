3f37a910c9299ff9843046ad21828efc5a2b38ee
Obioma <obeeomaa@gmail.com>
2024-02-12 10:16:46 +0100

    Add README content

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,42 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${data.licenseBadge} 
+  
+ 
+## Description 
+${data.description}
 
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license) 
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#questions)
+
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+
+This project is licensed under the ${data.license} license. 
+
+## Contributing
+${data.contributing}
+
+## Test
+${data.test}
+
+## Questions
+- GitHub: ${data.github}
+- Email: ${data.email}
 `;
 }
 
